761404d1a4209c48fa927b9dc8ecffa69f99b829
Qiuhan Zhu <vincentz@Qiuhans-MacBook-Pro.local>
2018-11-17 17:16:28 -0800

    add redux features to client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,8 @@
 import 'materialize-css/dist/css/materialize.min.css'
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-=======
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
->>>>>>> aa8143f38d3641d2751f571b6e1f3261ad2c1e10
 
 import Header from './Header';
 
@@ -16,7 +11,6 @@ const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 
-<<<<<<< HEAD
 class App extends Component {
     componentDidMount() {
         this.props.fetchUser();
@@ -39,21 +33,3 @@ class App extends Component {
 }
 
 export default connect(null, actions)(App);
-=======
-const App = () => {
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={DashBoard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
-};
-
-export default App;
->>>>>>> aa8143f38d3641d2751f571b6e1f3261ad2c1e10
--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-<<<<<<< HEAD
 import reduxThunk from 'redux-thunk';
-=======
->>>>>>> aa8143f38d3641d2751f571b6e1f3261ad2c1e10
 
 import App from './components/App';
 import reducers from './reducers';
 
-<<<<<<< HEAD
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-=======
-const store = createStore(reducers, {}, applyMiddleware());
->>>>>>> aa8143f38d3641d2751f571b6e1f3261ad2c1e10
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
